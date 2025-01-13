8627d852e6027f92f695f496bb5a713b27a30a6c
Syed Saad Ahmed <111363899+CosZmo77@users.noreply.github.com>
2025-01-13 14:56:19 +0530

    Update AdminContext.jsx

--- admin/src/Context/AdminContext.jsx
+++ admin/src/Context/AdminContext.jsx
@@ -8,7 +8,7 @@ const AdminContextProvider = (props) => {
   const [aToken, setAToken] = useState(
     localStorage.getItem("aToken") ? localStorage.getItem("aToken") : ""
   );
-  const backendUrl = "https://swiftcare-backend-ipoq.onrender.com";
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
   const [doctors, setDoctors] = useState([]);
   const [appointments, setAppointments] = useState([]);
   const [dashData, setDashData] = useState(false);
