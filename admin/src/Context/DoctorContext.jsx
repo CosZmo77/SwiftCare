f2f0f69b81f981440e2d5da137ea6f3f1a4ef7ab
Syed Saad Ahmed <111363899+CosZmo77@users.noreply.github.com>
2025-01-13 14:56:52 +0530

    Update DoctorContext.jsx

--- admin/src/Context/DoctorContext.jsx
+++ admin/src/Context/DoctorContext.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 export const DoctorContext = createContext();
 
 const DoctorContextProvider = (props) => {
-  const backendUrl = "https://swiftcare-backend-ipoq.onrender.com";
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
   const [dToken, setDToken] = useState(
     localStorage.getItem("dToken") ? localStorage.getItem("dToken") : ""
