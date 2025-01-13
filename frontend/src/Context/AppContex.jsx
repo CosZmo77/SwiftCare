3d0253496daad911a761aa81f630ac62d5622807
Syed Saad Ahmed <111363899+CosZmo77@users.noreply.github.com>
2025-01-13 12:02:54 +0530

    Update AppContex.jsx

--- frontend/src/Context/AppContex.jsx
+++ frontend/src/Context/AppContex.jsx
@@ -5,7 +5,7 @@ import { toast } from "react-toastify";
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = "https://swiftcare-backend-ipoq.onrender.com";
   const [doctors, setDoctors] = useState([]);
   const [token, setToken] = useState(
     localStorage.getItem("token") ? localStorage.getItem("token") : false
